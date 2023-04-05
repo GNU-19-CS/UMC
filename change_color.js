a939185c4ab2c209f49c92afdce44a6e649f09b0
GNU-19-CS <dbwlgns0923@naver.com>
2023-04-06 01:59:18 +0900

    use strict let const

--- change_color.js
+++ change_color.js
@@ -1,7 +1,9 @@
+'use strict';
+
 function changeBg() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const bgColor = "rgb(" + x + "," + y + "," + z + ")";
       document.body.style.background = bgColor;
   }
--- list.js
+++ list.js
@@ -1,5 +1,7 @@
-var isOpen1=false;
-var check1=document.querySelector("#list_1");
+'use strict';
+
+let isOpen1=false;
+const check1=document.querySelector("#list_1");
 check1.addEventListener("click", function(){
     if(isOpen1 == false){
       document.querySelector("#hidden_content1").style.display="block";
@@ -13,8 +15,8 @@ check1.addEventListener("click", function(){
     }
 });
 
-var isOpen2=false;
-var check2=document.querySelector("#list_2");
+let isOpen2=false;
+const check2=document.querySelector("#list_2");
 check2.addEventListener("click", function(){
     if(isOpen2 == false){
       document.querySelector("#hidden_content2").style.display="block";
@@ -28,8 +30,8 @@ check2.addEventListener("click", function(){
     }
 });
 
-var isOpen3=false;
-var check3=document.querySelector("#list_3");
+let isOpen3=false;
+const check3=document.querySelector("#list_3");
 check3.addEventListener("click", function(){
     if(isOpen3 == false){
       document.querySelector("#hidden_content3").style.display="block";
@@ -43,8 +45,8 @@ check3.addEventListener("click", function(){
     }
 });
 
-var isOpen4=false;
-var check4=document.querySelector("#list_4");
+let isOpen4=false;
+const check4=document.querySelector("#list_4");
 check4.addEventListener("click", function(){
     if(isOpen4 == false){
       document.querySelector("#hidden_content4").style.display="block";
@@ -58,8 +60,8 @@ check4.addEventListener("click", function(){
     }
 });
 
-var isOpen5=false;
-var check5=document.querySelector("#list_5");
+let isOpen5=false;
+const check5=document.querySelector("#list_5");
 check5.addEventListener("click", function(){
     if(isOpen5 == false){
       document.querySelector("#hidden_content5").style.display="block";
@@ -73,8 +75,8 @@ check5.addEventListener("click", function(){
     }
 });
 
-var isOpen6=false;
-var check6=document.querySelector("#list_6");
+let isOpen6=false;
+const check6=document.querySelector("#list_6");
 check6.addEventListener("click", function(){
     if(isOpen6 == false){
       document.querySelector("#hidden_content6").style.display="block";
@@ -88,8 +90,8 @@ check6.addEventListener("click", function(){
     }
 });
 
-var isOpen7=false;
-var check7=document.querySelector("#list_7");
+let isOpen7=false;
+const check7=document.querySelector("#list_7");
 check7.addEventListener("click", function(){
     if(isOpen7 == false){
       document.querySelector("#hidden_content7").style.display="block";
